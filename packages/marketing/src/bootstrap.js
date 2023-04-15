01e79295bb1147bd2a0a0544009ed554a8676424
Abhishek <abhishekpatel190494@gmail.com>
2023-04-15 23:28:54 +0200

    assembling cantainers and shared modules

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM  from 'react-dom';
+import App from './App'
+
+// mount function to start up the app
+
+const mount = (el) => {
+    ReactDOM.render(<App />, el);
+};
+
+// if we are in development and inisolation,
+// call mount immediately
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector('#_marketing-dev-root');
+
+    if (devRoot) {
+        mount(devRoot);
+    }
+}
+
+// running through container
+// export the mount function
+export {mount};
